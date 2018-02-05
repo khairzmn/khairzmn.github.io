c3d5c139979ec8f4325caf0ad22f0963e187b33e
Khairul Zaman <khairulzaman@Khairuls-MacBook-Pro.local>
2018-02-05 20:29:51 +0800

    Day 2 demo commit

--- newpagejs.js
+++ newpagejs.js
@@ -1,3 +1,15 @@
 console.log ("live transmission");
 
-var flintstones = ["fred","wilma","pebbles"];
+var avengers = ["hulk","thor","ironman"];
+
+//DAY 2 EXERCISE
+//use while statement to print out the element of the array
+//one on each line
+//it should work with an array of any length
+
+// Hint: (1)while (2)console.log (3)length of array (4)var to keep track of which element you are at (5)increment
+
+while (avengers < 3) {
+    text += "The name is " + avengers;
+    avengers++;
+}
