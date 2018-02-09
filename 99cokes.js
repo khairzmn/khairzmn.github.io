083c6e71b844e1ffaf2ca399b3402e4a388fbf09
Khairul Zaman <khairulzaman@Khairuls-MacBook-Pro.local>
2018-02-09 21:23:25 +0800

    bottles and form

--- 99cokes.js
+++ 99cokes.js
@@ -1,15 +1,40 @@
-console.log("we're live now");
+var mkBottle = function(value) {
+  var div = document.createElement('div');
+  for (var i = 0; i < value; i++) {
+      var img = document.createElement('img');
+      img.src = '/images/bottle.png';
+      div.appendChild(img);
+  }
+  return (div);
+}
 
-var coke = 99;
-while (coke > 0) {
-  var verse = [
-    coke + " bottles of coke on the wall,",
-    coke + " bottles of coke!",
-    "Take one down, pass it around",  
-    (coke - 1) + " bottles of coke on the wall!"
-  ].join("\n");
- 
-  console.log(verse);
- 
-  coke--;
+var mkText = function(text) {
+  // <div>text</div> 
+  // textContent
+  var div = document.createElement('div');
+  div.textContent = text;
+  //div.classList.remove('bigger');
+  // <div class='bigger'> 
+  div.classList.add('bigger');
+  return (div);
+}
+
+var song = document.querySelector('#song');
+
+for (var i = 99; i >= 0; i -= 1) {
+  var bottles = mkBottle(i);
+  song.appendChild(bottles);
+  var lyrics;
+  if (i > 1)
+      lyrics = i + ' bottles of beer on the wall, ' + i + ' bottles of beer. ' +
+              'Take one down and pass it around, ' + (i - 1) + ' bottles of beer on the wall.';
+  else if (i == 1)
+      lyrics = i + ' bottles of beer on the wall, ' + i + ' bottles of beer. ' +
+              'Take one down and pass it around, no more bottles of beer on the wall.';
+  else
+      lyrics = 'No more bottles of beer on the wall, no bottles of beer. ' +
+              'Go to the store and buy some more, 99 bottles of beer on the wall.'
+
+  var text = mkText(lyrics);
+  song.appendChild(text);
 }
